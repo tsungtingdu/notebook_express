ecfb17a5ede9d1395a09746305eef9f816eafb0c
canopus <tsungtingdu@gmail.com>
2018-02-04 14:34:20 +0800

    Connect to MongoDB

---
+++ models/notebook.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const noteSchema = new Schema({
+  name: {type: String, required: true},
+  title: {type: String, required: true},
+  note: {type: String, required: true}
+});
+
+const Note = mongoose.model('Note', noteSchema);
+
+module.exports = Notes;
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.listen(3000, () =>{
-  console.log('Listening on port 3000...');
+mongoose.connect('mongodb://canopus:000000@ds125068.mlab.com:25068/notebook-express');
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  app.listen(3000, () => {
+    console.log('Listening on port 3000...');
+  });
 });
